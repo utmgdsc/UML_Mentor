959c5965d9e358e738a9896868f74c8f6381e194
Vladmidir <vladgoran2004@gmail.com>
2024-02-08 07:06:57 -0500

    added data fetching. Needs further testing

--- client/src/pages/Challenge.tsx
+++ client/src/pages/Challenge.tsx
@@ -1,19 +1,57 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { Container, Row, Col, ButtonToolbar, Button } from "react-bootstrap";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+enum ChallengeDifficulties {
+    EASY,
+    MEDIUM,
+    HARD
+}
+
+type ChallengeDetails = {
+    title: string;
+    outcome: string;
+    difficulty: ChallengeDifficulties;
+    generalDescription: string;
+    keyPatterns: Array<string>;
+    expectedFunctionality: object;
+    usageScenarios: object;
+}
 
 const Challenge = () => {
 
     const [completed, setCompleted] = useState(true);
+    const [details, setDetails]  = useState<ChallengeDetails>();
     //fetch data about the challenge with the provided "id" here... (useEffect)
-    const id = useParams();
+    const { id } = useParams();
 
-    const challenge_details = {
-        "id": 12,
+    useEffect(() => {
+    
+        //NEEDS TESTING
+        //get the challenge details
+        fetch('/api/challenge/' + id).then((response) => {
+            if(!response.ok){
+                throw new Error(response.statusText);                
+            }
+            console.log(response);
+            return response.json() as Promise<{data : ChallengeDetails}>; //THIS LINE MAY CAUSE ERRORS. Need to test with proper server.
+        }).then((data) => {
+            console.log(data);
+            setDetails(data.data);
+            return;
+        }).catch((err) => {
+            console.log();
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            console.error("Failed fetching the challenge number: " + id + "\nError message: " + err.message)
+        });
+   }, [id])
+
+
+    const challenge_details: ChallengeDetails = {
         "title": "Hotel Room Booking System",
+        "difficulty": ChallengeDifficulties.MEDIUM,
         "outcome": "A system for managing hotel room reservations.",
         "keyPatterns": [
           "Factory Method pattern for creating different types of rooms",
@@ -32,19 +70,26 @@ const Challenge = () => {
         }
       };
 
+
+    //START FOR TESTING ONLY
+    if (details == undefined){
+        setDetails(challenge_details);  
+    } 
+    //END FOR TESTING ONLY
+    if (details != undefined)
     return(
     <Container>
         <section>
         <Row>
             <header className="text-center bg-secondary bg-gradient text-light p-5 my-4">
-                <h1>{challenge_details.title}</h1>
-                <h2>{challenge_details.outcome}</h2>
+                <h1>{details.title}</h1>
+                <h2>{details.outcome}</h2>
             </header> 
         </Row>
         <Row className="">
             <Col className="col-8">
                 <h3>Description:</h3>
-                <p>{challenge_details.generalDescription}</p>
+                <p>{details.generalDescription}</p>
             </Col>
             <Col className="text-center align-middle">
                 {completed && <div className="">
