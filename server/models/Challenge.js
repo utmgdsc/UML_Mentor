4858876127592218e0d7e591626e47b33548154e
okhamis <omar.khamis@mail.utoronto.ca>
2024-02-10 13:11:53 -0500

    updated challenege fields

--- server/models/Challenge.js
+++ server/models/Challenge.js
@@ -1,5 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
     const Challenge = sequelize.define('Challenge', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
         description: {
             type: DataTypes.TEXT,
             allowNull: false
@@ -7,8 +13,11 @@ module.exports = (sequelize, DataTypes) => {
         title: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        difficulty: {
+            type: DataTypes.STRING,
+            allowNull: false
         }
     });
     return Challenge;
-  };
-  
+};
