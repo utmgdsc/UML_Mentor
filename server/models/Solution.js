a575307d2fe111111b657f251904cfef700eec06
okhamis <omar.khamis@mail.utoronto.ca>
2024-02-10 13:42:07 -0500

    setup database

---
+++ server/models/Solution.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+    const Solution = sequelize.define('Solution', {
+        challengeId: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'Challenges', // name of Target model
+              key: 'id',
+            }
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'Users',
+              key: 'id',
+            }
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        title: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        diagram: {
+          type: DataTypes.STRING,
+          allowNull: true,
+          comment: "URL to the diagram image"
+      }
+    });
+    // Define associations
+    Solution.associate = function(models) {
+      Solution.belongsTo(models.User, { foreignKey: 'userId' });
+      Solution.belongsTo(models.Challenge, { foreignKey: 'challengeId' });
+      Solution.hasMany(models.Comment, { foreignKey: 'solutionId' });
+    };
+  
+    return Solution;
+  };
+  
