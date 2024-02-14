c95dc7e2cd90abfa1680072926a130c94ff1a18d
Vlad Yaremchuk <121591697+Vladmidir@users.noreply.github.com>
2024-02-14 17:58:27 -0500

    Connect client server (#23)
    * setup development proxy
    
    * setup production redirects
    
    * connected challenges route to the server
    
    * fixed Alex's typos
    
    * deleted proxy-middleware from client dependencies

--- server/controllers/ChallengeController.js
+++ server/controllers/ChallengeController.js
@@ -1,10 +1,37 @@
 const db = require("../models/index");
 const Challenge = db.Challenge;
 
-exports.get = async (req, res) => {
+exports.findOne = async (req, res) => {
 	try {
-        const challenges = await Challenge.findAll();
-        res.status(200).json(challenges);
+        const challenge_id = req.params.id;
+
+
+        // const challenge = await Challenge.findOne({
+        //     where: { 
+        //         id: challenge_id ,
+        //         // userId: req.user.id //Set up properly after done authentication
+        //     },
+        // });
+
+        //FOR TESTING
+        const challenge = {
+            "id": 1,
+            "difficulty": "Easy",
+            "title": "Simple Blogging Platform",
+            "outcome": "A basic platform for users to create and publish blog posts.",
+            "keyPatterns": ["Factory Method pattern for creating different types of blog posts."],
+            "generalDescription": "Creation and editing of blog posts, comment management, and basic user profiles.",
+            "expectedFunctionality": {
+                "CreatePost": "Create a new Text, Photo or Image post."
+            },
+            "usageScenarios": {
+                "TextPost": "Users can write and format text, add tags, and categorize their posts.",
+                "PhotoPost": "Users can upload images, create galleries, and add brief descriptions.",
+                "VideoPost": "Users can embed videos from platforms like YouTube or upload directly."
+            }
+        }
+
+        res.status(200).json(challenge);
     }
 	catch (error) {
         res.status(500).json({ error: error.message });
