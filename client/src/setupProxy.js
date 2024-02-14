66ac7113eae5e618b7b1b05e5b688d3f6c65f0b2
Vladmidir <vladgoran2004@gmail.com>
2024-02-14 15:19:32 -0500

    initial commit

---
+++ client/src/setupProxy.js
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://localhost:8080/api',
+      changeOrigin: true,
+    })
+  );
+};
