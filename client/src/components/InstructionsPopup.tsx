2030c9f50d77813dcfbd807a7cde6940a2bd6d8d
okhamis <omar.khamis@mail.utoronto.ca>
2024-03-05 13:34:03 -0500

    adding handling for images

--- client/src/components/InstructionsPopup.tsx
+++ client/src/components/InstructionsPopup.tsx
@@ -7,18 +7,38 @@ type InstructionsPopupProps = {
 };
 
 const instructions = [
-  { title: 'Grasp the Challenge', body: 'Begin by thoroughly understanding the problem statement. A clear grasp of the situation is crucial for crafting an effective solution.' },
-  { title: 'Difficulty of Challenge', body: 'The stars shown on the challenge indicates the difficulty. The more starts the more difficulty' },
-  { title: 'Design Your Solution', body: 'Use our integrated drawing tool to diagram your solution. A well-thought-out design is key to addressing the challenge effectively.' },
-  { title: 'Submit Your Work', body: 'Once you are satisfied with your design, go ahead and submit it. Your innovative solutions are what we looking forward to seeing.' },
-  { title: 'Collaborate and Feedback', body: 'Engage with your peers by reviewing their solutions. Constructive feedback fosters learning and innovation within our community.' }
-  // add more as needed
-];
+    { 
+      title: 'Understand the Problem',
+      body: 'Begin by thoroughly understanding the problem statement. A clear grasp of the situation is crucial for crafting an effective solution.',
+      imgSrc: '../assets/InstructionsDesc.png'
+    },
+    { 
+      title: 'Check the Difficulty',
+      body: 'The number of stars indicates the difficulty of the challenge. More stars mean a greater challenge!',
+      imgSrc: '../assets/InstructionsDifficulty.png'
+    },
+    { 
+      title: 'Design Your Solution',
+      body: 'Use our integrated drawing tool to diagram your solution. A well-thought-out design is key to addressing the challenge effectively.',
+      imgSrc: '../assets/InstructionsEditor.png'
+    },
+    { 
+      title: 'Submit Your Work',
+      body: 'Once you are satisfied with your design, go ahead and submit it. Your innovative solutions are what were looking forward to seeing.',
+      imgSrc: '../assets/InstructionsPostSol.png'
+    },
+    { 
+      title: 'Collaborate and Provide Feedback',
+      body: 'Engage with your peers by reviewing their solutions. Constructive feedback fosters learning and innovation within our community.',
+      imgSrc: '../assets/InstructionsViewSol.png'
+    }
+    // Add more instructions as needed
+  ];
+  
 
 
 function InstructionsPopup({ show, handleClose }: InstructionsPopupProps) {
   const [currentInstructionIndex, setCurrentInstructionIndex] = useState(0);
-  const instructionCount = `${currentInstructionIndex + 1}/${instructions.length}`;
 
   const handleNext = () => {
     setCurrentInstructionIndex((prevIndex) => (prevIndex + 1) % instructions.length);
@@ -29,9 +49,19 @@ function InstructionsPopup({ show, handleClose }: InstructionsPopupProps) {
   };
 
   return (
-    <Modal show={show} onHide={handleClose} centered size="xl">
+    <Modal show={show} onHide={handleClose} centered size="lg">
       <Card>
-        <Card.Header>{instructions[currentInstructionIndex].title}</Card.Header>
+        <Card.Header>
+        {instructions[currentInstructionIndex].title}
+        <span className="float-right">
+            {currentInstructionIndex + 1}/{instructions.length}
+        </span>
+        </Card.Header>
+        <Card.Img 
+        variant="top" 
+        src={instructions[currentInstructionIndex].imgSrc} 
+        style={{ maxHeight: '200px', width: 'auto' }}
+        />
         <Card.Body>
           <Card.Text>{instructions[currentInstructionIndex].body}</Card.Text>
           <div className="d-flex justify-content-between">
