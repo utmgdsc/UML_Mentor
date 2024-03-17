60966b89922d68c5505fce5309acc3c8f9d77550
okhamis <omar.khamis@mail.utoronto.ca>
2024-03-17 13:33:05 -0400

    added auth middleware & updated roles

--- server/models/User.js
+++ server/models/User.js
@@ -15,18 +15,18 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
             unique: true
         },
-        roleId: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'Roles',
-                key: 'id',
-              }
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: 'user', // default role is 'user'
+            validate: {
+                isIn: [['user', 'admin']], // Ensures the role is either 'user' or 'admin'
+            }
         },
         score: DataTypes.INTEGER
     });
     User.associate = function(models) {
         // Define associations
-        User.belongsTo(models.Role, { foreignKey: 'roleId' });
         User.hasMany(models.Solution, { foreignKey: 'userId' });
         User.hasMany(models.Comment, { foreignKey: 'userId' });
       };
---
+++ server/routes/AuthenticationMiddleware.js
@@ -0,0 +1,23 @@
+module.exports = (User) => async (req, res, next) => {
+    try {
+        const utorid = req.headers.utorid;
+        let user = await User.findOne({ where: { username: utorid } });
+
+        if (!user) {
+            // can be adjusted to make test admin users
+            user = await User.create({
+                username: utorid,
+                passwordHash: 'defaultPasswordHash', // i am not sure if we need this for shibboleth
+                email: req.headers.email, // can we get the email frrom shibboleth?
+                role: 'user',
+            });
+        }
+
+        // Attach the user to the request object for use in subsequent middleware or handlers
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error('Error ensuring user exists:', error);
+        res.status(500).send({ error: 'Internal Server Error' });
+    }
+};
