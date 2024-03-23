7a99413a2cca91750bad73549cc59923bac5d689
Eren <gerensuner@gmail.com>
2024-03-22 23:16:22 -0400

    Gets feedback without image

--- server/controllers/SolutionController.js
+++ server/controllers/SolutionController.js
@@ -1,8 +1,10 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const STORAGE_CONFIG = require("../storage_config.json");
 const db = require("../models/index");
+const { HandledError } = require("../routes/ErrorHandlingMiddleware");
 const Solution = db.Solution;
 const Comment = db.Comment;
+const Challenge = db.Challenge;
 
 exports.getAll = async (req, res) => {
   const solutions = await Solution.findAll();
@@ -19,7 +21,7 @@ exports.getComments = async (req, res) => {
   const { id } = req.params;
   const solution = await Solution.findByPk(id);
   if (!solution) {
-    return res.status(404).json({ message: "Solution not found" });
+    throw HandledError(404, "Solution not found");
   }
   const comments = await solution.getComments();
   res.status(200).json(comments);
@@ -66,9 +68,7 @@ exports.edit = async (req, res) => {
 
     const solution = await Solution.findByPk(id);
     // TODO: construct a path in a better way
-    fs.unlink(`${STORAGE_CONFIG.location}/${solution.diagram}`, (err) =>
-      console.log(err),
-    );
+    await fs.unlink(`${STORAGE_CONFIG.location}/${solution.diagram}`);
   }
 
   if (file) {
@@ -87,4 +87,53 @@ exports.delete = async (req, res) => {
   const { id } = req.params;
   await Solution.destroy({ where: { id } });
   res.status(204).send();
+};
+
+const { ChatOpenAI } = require("@langchain/openai");
+const { ChatPromptTemplate } = require("@langchain/core/prompts");
+const { StringOutputParser } = require("@langchain/core/output_parsers");
+
+exports.getAIFeedback = async (req, res) => {
+  const { id } = req.params;
+  const solution = await Solution.findByPk(id);
+
+  const challenge = await Challenge.findByPk(solution.challengeId);
+  const diagramBuffer = await fs.readFile(
+    `${STORAGE_CONFIG.location}/${solution.diagram}`,
+  );
+  const diagramb64 = diagramBuffer.toString("base64");
+
+  console.log("Diagram:", diagramb64);
+
+  const prompt = ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      "You are a teaching assistant for a software architecture course. You are excellent at providing" +
+        " feedback to solutions written by users to software architecture challenges, including questions about UML" +
+        " diagrams, software architecture patterns and SOLID principles. The images you receive will be of UML" +
+        " diagrams. Provide" +
+        " extensive and helpful feedback as a teaching assistant.",
+    ],
+    [
+      "user",
+      "Challenge: {challenge_title}\n{challenge_description}\n" +
+        "\nSolution: {solution_title}\n{solution_description}",
+    ],
+  ]);
+  const model = new ChatOpenAI({ modelName: "gpt-4-vision-preview" });
+  const outputParser = new StringOutputParser();
+
+  const chain = prompt.pipe(model).pipe(outputParser);
+
+  const response = await chain.invoke({
+    challenge_title: challenge.title,
+    challenge_description: challenge.description,
+    solution_title: solution.title,
+    solution_description: solution.description,
+  });
+
+  res.status(200).json({
+    status: "ok",
+    response,
+  });
 };
