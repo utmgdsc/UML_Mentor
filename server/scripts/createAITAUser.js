9668f852909229e8acb5e524f06943eeafca3153
Eren <gerensuner@gmail.com>
2024-04-03 15:55:24 -0400

    Update createAITAUser.js

--- server/scripts/createAITAUser.js
+++ server/scripts/createAITAUser.js
@@ -2,6 +2,10 @@ const db = require("../models/index");
 const User = db.User;
 
 module.exports = async () => {
+  const AITA = await User.findByPk(-13);
+  if (AITA) {
+    return;
+  }
   await User.create({
     id: -13,
     username: "AI TA",
