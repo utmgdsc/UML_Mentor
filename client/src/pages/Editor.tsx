606a2fed72e4918615ed2365d96e45d682bef662
Rajveer Singh Anand <rajveersinghanand.ca@gmail.com>
2024-10-01 23:03:44 -0400

    Working basic version of react flow with some features. Need to add templates

--- client/src/pages/Editor.tsx
+++ client/src/pages/Editor.tsx
@@ -1,272 +1,44 @@
-import { useEffect, useRef, useState, useCallback } from "react";
-import { DrawIoEmbed, DrawIoEmbedRef } from "react-drawio";
-import { useQuery } from "../hooks/useQuery";
+import React, { useCallback } from 'react';
+import {
+    ReactFlow,
+    MiniMap,
+    Controls,
+    Background,
+    useNodesState,
+    useEdgesState,
+    addEdge,
+} from '@xyflow/react';
 
-const CONFIG = {
-  defaultEdgeStyle: {
-    endSize: 12,
-    startSize: 12,
-    edgeStyle: "orthogonalEdgeStyle",
-    rounded: 1,
-    curved: 0,
-  },
-  enableCustomLibraries: false,
-  appendCustomLibraries: false,
-  expandLibraries: true,
-  defaultLibraries: "uml",
-  override: false,
-};
+import '@xyflow/react/dist/style.css';
 
-const Editor = () => {
-  const drawioRef = useRef<DrawIoEmbedRef>(null);
-  const diagramData = useRef<string | undefined>(undefined);
-  //TODO: Allow the user to change the diagram name (super low priority)
-  const [diagramName, setDiagramName] = useState<string | null>(null);
-  const diagramNameRef = useRef<string | null>(null); //Crutch for the handleSave function
-  const diagramId = useRef<string | null>(null);
+const initialNodes = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-  const query = useQuery();
+export default function App() {
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  function doExport() {
-    if (drawioRef.current) {
-      drawioRef.current.exportDiagram({
-        format: "xmlpng",
-      });
-    }
-  }
+    const onConnect = useCallback(
+        (params) => setEdges((eds) => addEdge(params, eds)),
+        [setEdges],
+    );
 
-
-  //autosave (export) the diagram every 20 seconds (ONLY FOR DIAGRAMS IN PROGRESS, NOT EDITING EXISTING SOLUTIONS)
-  useEffect(() => {
-    if (query.get("type") !== "challenge") return;
-    const interval = setInterval(() => {
-      // console.log("autosaving");
-      doExport();
-    }, 20000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [query]);
-
-  useEffect(() => {
-    // If query.get("type") === "challenge" then we need to fetch the SolutionInProgress from the server
-    // by challengeId and userId. If the SolutionInProgress exists, we need to load it into the editor (diagram + title).
-    // Otherwise we create a new diagram + title, load it into the editor and make a SolutionInProgress entry in the db.
-    if (query.get("type") === "challenge") {
-      const challengeId = query.get("id");
-
-      //fetch an existing diagram from the server and load it into the editor
-      //If the diagram is not found, create a new diagram and save it to the server
-      fetch("/api/inprogress/challenge/" + challengeId) //GET
-      .then(response => { 
-        if(response.status === 404) {
-          throw new Error("404");
-        }
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-        return response.json() as Promise<{diagram: string, title: string, id: string}>
-      })
-      .then(data => {
-        // console.log(data);
-        diagramData.current = data.diagram;
-        diagramId.current = data.id;
-        setDiagramName(data.title); // IMPORTANT: This state update is required to actually load the diagram
-        diagramNameRef.current = data.title; //Crutch for the handleSave function
-        // console.log("Loaded diagram: " + diagramData.current + " with id: " + diagramId.current + " and name: " + diagramNameRef.current);
-      })
-      .catch((error: Error) => {
-        // console.log("Error: " + error.message);
-        //if the solution in progress has not been created yet, create a new entry in the database
-        if(error.message === "404") {
-          //fetch the challenge details from the server to get the title
-          fetch("/api/challenges/" + challengeId).then(response => { //GET
-            if (!response.ok) {
-              throw new Error(response.statusText);
-            }
-            return response.json() as Promise<{title: string}>
-          }).then(data => {
-            setDiagramName(data.title + " Diagram");
-            diagramNameRef.current = data.title + " Diagram"; //Crutch for the handleSave function
-            //create a new diagram and save it to the server
-            return fetch("/api/inprogress" , { //POST
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json"
-              },
-              body: JSON.stringify({
-                title: diagramNameRef.current, 
-                challengeId: challengeId, 
-                xml: ""})
-            });
-          }).then(response => {
-            if (!response.ok) {
-              throw new Error(response.statusText);
-            }
-            return response.json() as Promise<{diagram: string, title: string, id: string}>
-          }).then(data => {
-            // console.log("Created new diagram with id: " + data.id + " and title: " + data.title);
-            diagramData.current = data.diagram;
-            diagramId.current = data.id;
-            setDiagramName(data.title);
-            diagramNameRef.current = data.title;
-            setTimeout(() => {
-              doExport();
-            }, 1000);
-          }).catch((error) => {
-            console.error(error);
-          });
-        } else {
-          //otherwise, log the error
-          console.error(error);
-        }
-        
-      });
-    }   
-    else if (query.get("type") === "solution") {
-      //TODO: Load the existing solution diagram into the editor. Do not autosave it.
-
-      // If query.get("type") === "solution" then we need to fetch the Solution from the server by solutionId.
-      // We load the solution into the editor. The user can then confirm or drop changes.
-      // Add a note for editing solution diagrams that it should only be used to add minor changes.
-    } else {
-      //Redirect to error page
-      //TODO: implement this properly
-      // return redirect("/error");
-    }
-  },[query] );
-
-
-  /**
-   * Save the diagram to the server by sending a PUT request to /api/solutions/inprogress with the diagram data.
-   */
-  const handleSave = useCallback(
-    (data: {
-      event: string;
-      xml: string;
-      data: string;
-      message: { exit?: boolean };
-    }) => {
-      //make sure to only process "export" events
-      if (data.message.exit == true) return;
-
-      const rawData = data.data;
-      diagramData.current = rawData.substring(rawData.indexOf(",") + 1); //trim the data:image/png;base64, part
-
-      if (diagramId.current !== null && diagramNameRef.current !== null) {
-        // console.log("Saving diagram: " + diagramData.current +
-        // " with id: " + diagramId.current +
-        // " and name: " + diagramNameRef.current +
-        // "on event" + data.event);
-        // console.log(data.message);
-
-      fetch("/api/inprogress/" + diagramId.current, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          xml: diagramData.current, 
-          title: diagramNameRef.current, 
-          challengeId: query.get("id") //NOTE: This only works for challenges in progress
-        }),
-        // keepalive: true, // To allow saves when the tab is closed LIMITS REQUEST SIZE TO 65KB
-      })
-      .then(response => {
-        console.log(response);
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-        return response.json() as Promise<{id: string}>
-      })
-      .then(data => {
-        // console.log('Success:', data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-    }
-  },[query, diagramName]);
-
-
-  return (
-    <div>
-      <h1 className="h2">{diagramName}</h1>
-      <div style={{height: "100vh"}}>
-        < DrawIoEmbed 
-          ref={drawioRef}
-          configuration={CONFIG}
-          exportFormat="xmlpng"
-          urlParameters={
-            {
-              // ui: "atlas",
-              spin: true,
-              // modified: true,
-              // keepmodified: true,
-              libraries: true,
-              noSaveBtn: false,
-              saveAndExit: false,
-              noExitBtn: false
-            }
-          }
-          onExport={(data) => {handleSave(data)}} //Note: it says the type is wrong, but it is not. 
-          onSave={(data) => {
-            // console.log(data);
-            // If the save is triggered by another event, then do nothing
-            if(data.parentEvent !== "save") {
-              return;
-            }
-            doExport();
-          }}
-          onLoad={(data) => {
-            // If the xml is null, then we need to wait and reload the diagram from diagramData
-            if (data.xml === null) {
-              // console.log("Waiting for diagramData to be initialized");
-              setTimeout(() => {
-                if (drawioRef.current !== null && diagramData.current !== undefined){
-                  
-                  drawioRef.current?.load({
-                    xmlpng: diagramData.current
-                  });
-                  
-                  console.log("Loaded diagram from stash");
-                }
-                else {
-                  console.log("No diagram data found");
-                }
-              }, 100);
-            }  
-          }}
-          onClose={(data) => {
-            // console.log(data);
-            // If the exit is triggered by another event, then do nothing
-            if(data.parentEvent) {
-              return;
-            }
-            window.close();
-
-                        // Save and exit. Timeout to make sure the save request is sent before the tab is closed.
-                        // doExport();
-                        // setTimeout(() => {
-                        //     window.close();
-                        // }, 1000);
-                    }}
-                    />
-            </div>
-            {/* TESTING ONLY
-            <button onClick={ () => {
-                if (drawioRef.current !== null && diagramData.current !== undefined){
-                    drawioRef.current?.load({
-                        xmlpng: diagramData.current
-                    });
-                    console.log("Loaded diagram from stash");
-                }
-                else {
-                    console.log("No diagram data found");
-                }
-            }}>Import Diagram</button> */}
-    </div>
-  );
-};
-
-export default Editor;
+    return (
+        <div style={{ width: '100vw', height: '100vh' }}>
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+            >
+                <Controls />
+                <MiniMap />
+                <Background variant="dots" gap={12} size={1} />
+            </ReactFlow>
+        </div>
+    );
+}
